54748f525bbbb8d4f2f8bb64c21a4b6dd6b8fd80
Ayushi Mehrotra <ayushi.m006@gmail.com>
2026-03-06 02:19:01 +0000

    more results

--- scripts/qwen_hh-single.sh
+++ scripts/qwen_hh-single.sh
@@ -17,51 +17,51 @@ for pair in "${pairs[@]}"; do
   
   for algo in "${algos[@]}"; do
       pip install nnsight==0.4.11
-      python run.py --model_id "$model_id" \
-                    --batch_size 4 \
-                    --patch_algo "$algo" \
-                    --source $source \
-                    --base  $base \
-                    --device "$device" \
-                    --patch_model
-      python run.py --model_id "$model_id" \
-                    --batch_size 1 \
-                    --patch_algo "$algo" \
-                    --source $source \
-                    --base  $base \
-                    --device "$device" \
-                    --eval_model \
-                    --eval_test  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-test.jsonl"\
-                    --steering \
-                    --ablation steer \
-                    --steering_add_path  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmful-single-desired-all.jsonl" \
-                    --steering_sub_path "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-desired-all.jsonl"
+      # python run.py --model_id "$model_id" \
+      #               --batch_size 4 \
+      #               --patch_algo "$algo" \
+      #               --source $source \
+      #               --base  $base \
+      #               --device "$device" \
+      #               --patch_model
+      # python run.py --model_id "$model_id" \
+      #               --batch_size 1 \
+      #               --patch_algo "$algo" \
+      #               --source $source \
+      #               --base  $base \
+      #               --device "$device" \
+      #               --eval_model \
+      #               --eval_test  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-test.jsonl"\
+      #               --steering \
+      #               --ablation steer \
+      #               --steering_add_path  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmful-single-desired-all.jsonl" \
+      #               --steering_sub_path "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-desired-all.jsonl"
 
-      python run.py --model_id "$model_id" \
-                    --batch_size 4 \
-                    --patch_algo "$algo" \
-                    --source $source \
-                    --base  $base \
-                    --device "$device" \
-                    --eval_model \
-                    --eval_test  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-test.jsonl"\
-                    --steering \
-                    --ablation steer \
-                    --steering_add_path  "/workspace/gcm-interp/data/${model_name}/harmful-long/harmful-long-desired-all.jsonl" \
-                    --steering_sub_path "/workspace/gcm-interp/data/${model_name}/harmful-long/harmless-desired-all.jsonl"
+      # python run.py --model_id "$model_id" \
+      #               --batch_size 4 \
+      #               --patch_algo "$algo" \
+      #               --source $source \
+      #               --base  $base \
+      #               --device "$device" \
+      #               --eval_model \
+      #               --eval_test  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-test.jsonl"\
+      #               --steering \
+      #               --ablation steer \
+      #               --steering_add_path  "/workspace/gcm-interp/data/${model_name}/harmful-long/harmful-long-desired-all.jsonl" \
+      #               --steering_sub_path "/workspace/gcm-interp/data/${model_name}/harmful-long/harmless-desired-all.jsonl"
 
-      python run.py --model_id "$model_id" \
-                    --batch_size 4 \
-                    --patch_algo "$algo" \
-                    --source $source \
-                    --base  $base \
-                    --device "$device" \
-                    --eval_model \
-                    --eval_test  "/workspace/gcm-interp/data/${model_name}/harmful-long/harmless-test.jsonl"\
-                    --steering \
-                    --ablation steer \
-                    --steering_add_path  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmful-single-desired-all.jsonl" \
-                    --steering_sub_path "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-desired-all.jsonl"
+      # python run.py --model_id "$model_id" \
+      #               --batch_size 4 \
+      #               --patch_algo "$algo" \
+      #               --source $source \
+      #               --base  $base \
+      #               --device "$device" \
+      #               --eval_model \
+      #               --eval_test  "/workspace/gcm-interp/data/${model_name}/harmful-long/harmless-test.jsonl"\
+      #               --steering \
+      #               --ablation steer \
+      #               --steering_add_path  "/workspace/gcm-interp/data/${model_name}/harmful-single/harmful-single-desired-all.jsonl" \
+      #               --steering_sub_path "/workspace/gcm-interp/data/${model_name}/harmful-single/harmless-desired-all.jsonl"
 
       python run.py --model_id "$model_id" \
                     --batch_size 4 \
